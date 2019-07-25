dcee6044c3df7673a62223a958ce5214a8b97864
Matt Valentine-House <matt@eightbitraptor.com>
2019-07-25 16:11:32 +0100

    Make the build gem script operate on absolute paths
    * It makes the sed command easier to read and more intention revealing
    * It means the script can be run from anywhere

--- packages-ruby/polaris_icons/scripts/build-gem.sh
+++ packages-ruby/polaris_icons/scripts/build-gem.sh
@@ -1,10 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p ./images/pdf/
-for file in ../../packages/polaris-icons-raw/icons/polaris/*.svg; do
-  rsvg-convert -f pdf -o `echo $file | sed -e 's/.svg$/.pdf/' -e 's/..\/..\/packages\/polaris-icons-raw\/icons\/polaris\//images\/pdf\//'` "$file"
+function abs_path {
+  old_pwd=$(pwd)
+  cd "$1" && full_path=$(pwd)
+  cd "$old_pwd"
+  echo "$full_path"
+}
+
+
+function recursive_find_file {
+  path=$(abs_path "$1")
+  while [[ "$path" != "" && ! -e "$path/$2" ]]; do
+    path=${path%/*}
+  done
+  echo "$path"
+}
+
+script_dir=$(abs_path "$(dirname "${BASH_SOURCE[0]}")")
+gem_root=$(recursive_find_file "$script_dir" "polaris_icons.gemspec")
+proj_root=$(recursive_find_file "$script_dir" "dev.yml")
+
+mkdir -p "$gem_root/images/pdf/"
+
+for file in "$proj_root"/packages/polaris-icons-raw/icons/polaris/*.svg; do
+  rsvg-convert -f pdf -o $(echo "$file" | sed -e 's/.svg$/.pdf/' -e "s#$proj_root/packages/polaris-icons-raw/icons/polaris\/#$gem_root/images/pdf/#") "$file"
   echo "Converted $file"
 done
 
-bundle exec rake build
+cd $gem_root && bundle exec rake build
