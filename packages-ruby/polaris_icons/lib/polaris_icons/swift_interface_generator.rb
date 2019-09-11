d20a4e6ff88829b4c8fb90736cd4fdb2912f1b92
Pedro Piñera <pedro@ppinera.es>
2019-09-11 14:09:12 +0200

    Add SwiftInterfaceGenerator utility

---
+++ packages-ruby/polaris_icons/lib/polaris_icons/swift_interface_generator.rb
@@ -0,0 +1,50 @@
+require 'open3'
+require 'tmpdir'
+require 'yaml'
+
+module PolarisIcons
+  class SwiftInterfaceGenerator
+    SwiftGenNotFound = Class.new(StandardError)
+
+    def generate(asset_catalogs_dir:)
+      raise SwiftGenNotFound unless swiftgen_available?
+
+      Dir.mktmpdir do |tmp_dir|
+        # Write config
+        swiftgen_config = swiftgen_config(asset_catalogs_dir: asset_catalogs_dir)
+        swiftgen_config_path = File.join(tmp_dir, "swiftgen.yml")
+
+        # Generate
+        File.write(swiftgen_config_path, swiftgen_config.to_yaml)
+        PolarisIcons.execute(
+          "swiftgen", "config", "run",
+          "--config", swiftgen_config_path
+        )
+      end
+    end
+
+    private
+
+    def swiftgen_config(asset_catalogs_dir:)
+      {
+        "xcassets" => {
+          "inputs" => Dir.glob(File.join(asset_catalogs_dir, "*")),
+          "outputs" => [
+            {
+              "templateName" => "swift4",
+              "output" => File.join(asset_catalogs_dir, "PolarisIcons.swift"),
+              "params" => {
+                "enumName" => "PolarisIcons"
+              }
+            }
+          ]
+        }
+      }
+    end
+
+    def swiftgen_available?
+      _, status = Open3.capture2("which", "swiftgen")
+      status.success?
+    end
+  end
+end
