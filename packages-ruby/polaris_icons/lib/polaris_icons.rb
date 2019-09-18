2385294bd09e471d9fdd26ba802e663f7727345f
Pedro Piñera <pedro@ppinera.es>
2019-09-18 11:01:36 +0200

    Don't generate the Swift interface

--- packages-ruby/polaris_icons/lib/polaris_icons.rb
+++ packages-ruby/polaris_icons/lib/polaris_icons.rb
@@ -1,6 +1,5 @@
 module PolarisIcons
   autoload :AssetCatalog, 'polaris_icons/asset_catalog'
-  autoload :SwiftInterfaceGenerator, 'polaris_icons/swift_interface_generator'
 
   def self.execute(*command)
     _, error, status = Open3.capture3(*command)
