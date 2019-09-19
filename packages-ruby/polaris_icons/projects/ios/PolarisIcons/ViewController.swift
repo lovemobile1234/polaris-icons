8ca7d9de42647e783cffdf8840f71e2de0baa543
Pedro Piñera <pedro@ppinera.es>
2019-09-19 17:26:05 +0200

    Reduce the icon size

--- packages-ruby/polaris_icons/projects/ios/PolarisIcons/ViewController.swift
+++ packages-ruby/polaris_icons/projects/ios/PolarisIcons/ViewController.swift
@@ -86,7 +86,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: 100)
+        return CGSize(width: 50, height: 50)
     }
 
 }
